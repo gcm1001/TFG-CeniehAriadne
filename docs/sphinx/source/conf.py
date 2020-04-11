a4f61c06d257ce7bb8542912380b09d0e8254dc9
Gon <gcm1001@alu.ubu.es>
2020-04-11 19:34:20 +0200

    Update Index & Add logo & Update LaTeX #34

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -56,6 +56,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = '_static/images/ubucenieh.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
