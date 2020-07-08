b7a52e0be96e3d00e68c89da8b0f4afe09dedf5d
Gon <gcm1001@alu.ubu.es>
2020-07-08 20:08:42 +0200

    Updated "IsPartOf" plugin #64

---
+++ omeka/plugins/AutoDublinCore/config_form.php
@@ -0,0 +1,28 @@
+<?php $view = get_view(); ?>
+<fieldset>
+<legend><?=  htmlspecialchars(__("Automatic Updates")); ?></legend>
+<div class="field">
+    <div class="two columns alpha">
+        <label for="sync"><?=  htmlspecialchars(__("Is Part Of")); ?></label>
+    </div>
+    <div class="inputs five columns omega">
+        <p class="explanation"><?=  htmlspecialchars(__("This will automatically update the 'Is Part Of' element with the identifier of the collection associated with the item. The 'Identifier' element of the collection is also automatically updated.")); ?></p>
+        <?= $view->formCheckbox('auto_ispartof', true,
+            array('checked' => (boolean) get_option('auto_ispartof')));
+        ?>
+    </div>
+</div>
+<div class="field">
+    <div class="two columns alpha">
+        <label for="sync"><?=  htmlspecialchars(__("Source (DSpace Platforms)")); ?></label>
+    </div>
+    <div class="inputs five columns omega">
+        <p class="explanation"><?= __("Handle link (e.g 'https://cir.cenieh.es/handle/')."); ?></p>
+        <?= $view->formText('auto_source_link', get_option('auto_source_link')); ?>
+        <p class="explanation"><?=  htmlspecialchars(__("This will automatically update the 'Source' element with the DSpace URI.")); ?></p>
+        <?= $view->formCheckbox('auto_source', true,
+            array('checked' => (boolean) get_option('auto_source')));
+        ?>
+    </div>
+</div>
+</fieldset>
--- omeka/plugins/IsPartOfCollection/tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('IS_PART_OF_COLLECTION_DIR', dirname(dirname(__FILE__)));
-require_once dirname(dirname(IS_PART_OF_COLLECTION_DIR)) . '/application/tests/bootstrap.php';
-require_once 'IsPartOfCollection_Test_AppTestCase.php';
